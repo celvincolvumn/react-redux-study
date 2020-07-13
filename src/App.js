3e64e210eae39b21530ef61d95f0d4cad7f7ad27
Kevin Song <celvincolvumn@gmail.com>
2020-07-13 21:29:33 +0900

    create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+
+import AddNumberBox from "./AddNumberBox";
+import DispalyNumberBox from "./DispalyNumberBox";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Root</h1>
+      <h1>Wrapper</h1>
+      <AddNumberBox></AddNumberBox>
+      <DispalyNumberBox></DispalyNumberBox>
     </div>
   );
 }
---
+++ src/DispalyNumberBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DisplayNumber from "./DisplayNumber";
+
+function DisplayNumberBox() {
+  return (
+    <div>
+      <h1>Display Number Box</h1>
+      <DisplayNumber></DisplayNumber>
+    </div>
+  );
+}
+
+export default DisplayNumberBox;
